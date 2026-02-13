c786f1e0d1b3293bd009d4c361bfaf28d5c7f079
Rong Liu <liur180855@gmail.com>
2026-02-12 20:33:59 -0600

    Delete hello_world_controller.js

--- hello_world_controller.js
+++
@@ -1,9 +0,0 @@
-angular.module('HelloWorldApp', [])
-   .controller('HelloWorldController', function($scope) {
-        
-        $scope.message = "";
-        $scope.booking = function(){
-            $scope.message = "we are picking you are at "+ $scope.pickup +" and drop you off at " 
-            +$scope.destination+ " on "+ document.getElementById('daterange').value+ " " +$scope.selectedTime+"am";
-        }
-});
